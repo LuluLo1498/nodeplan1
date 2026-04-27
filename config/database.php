d86d7eb34f56a93bac334d6520e5038660bcb240
LuluLo1498 <lourdesboixescandell@gmail.com>
2026-04-27 22:34:15 +0200

    Ajustando base datos para Vercel

--- config/database.php
+++ config/database.php
@@ -86,18 +86,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            // Intentará usar DATABASE_URL (Vercel), si no existe usaría DB_URL
-            'url' => env('DATABASE_URL') ?: env('DB_URL'), 
+            'url' => env('POSTGRES_URL') ?: env('DATABASE_URL'), // Vercel inyecta estas dos
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'require', // Neon requiere SSL para conectar desde fuera
+            'sslmode' => 'require', // OBLIGATORIO para Neon
         ],
 
         'sqlsrv' => [
